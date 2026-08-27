e4adb6e932f420d204a96f3d41df33d5923c014f
Muhammad Al-Nashmi <57289455+saqs1@users.noreply.github.com>
2026-08-28 02:33:45 +0300

    Require original S2E before OLIVER Phase123 v0.1.2

--- phase123_embedded/OliverPhase123Plugin.cs
+++ phase123_embedded/OliverPhase123Plugin.cs
@@ -1,7 +1,8 @@
 using BepInEx;
 using BepInEx.Unity.IL2CPP;
 
-[BepInPlugin("oliver.tik.s2e.phase123", "OLIVER S2E Phase123", "0.1.1")]
+[BepInPlugin("oliver.tik.s2e.phase123", "OLIVER S2E Phase123", "0.1.2")]
+[BepInDependency("s2e-supermarket-simulator", BepInDependency.DependencyFlags.HardDependency)]
 public sealed class OliverPhase123Plugin : BasePlugin
 {
     public override void Load()
