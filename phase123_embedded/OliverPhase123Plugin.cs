55e57918c95cdfaa333ee16b4daa41755e1fd958
Muhammad Al-Nashmi <57289455+saqs1@users.noreply.github.com>
2026-08-28 02:27:11 +0300

    Defer OLIVER Phase123 initialization until original S2E is loaded

--- phase123_embedded/OliverPhase123Plugin.cs
+++ phase123_embedded/OliverPhase123Plugin.cs
@@ -1,11 +1,11 @@
 using BepInEx;
 using BepInEx.Unity.IL2CPP;
 
-[BepInPlugin("oliver.tik.s2e.phase123", "OLIVER S2E Phase123", "0.1.0")]
+[BepInPlugin("oliver.tik.s2e.phase123", "OLIVER S2E Phase123", "0.1.1")]
 public sealed class OliverPhase123Plugin : BasePlugin
 {
     public override void Load()
     {
-        OliverBootstrap.Init();
+        OliverBootstrap.BeginDeferred();
     }
 }
