9c2aec0115316127a216136ebc56c79a421337d5
Muhammad Al-Nashmi <57289455+saqs1@users.noreply.github.com>
2026-08-28 03:01:22 +0300

    Bump Phase123 to v0.1.6 after Unicode font fix

--- phase123_embedded/OliverPhase123Plugin.cs
+++ phase123_embedded/OliverPhase123Plugin.cs
@@ -1,7 +1,7 @@
 using BepInEx;
 using BepInEx.Unity.IL2CPP;
 
-[BepInPlugin("oliver.tik.s2e.phase123", "OLIVER S2E Phase123", "0.1.5")]
+[BepInPlugin("oliver.tik.s2e.phase123", "OLIVER S2E Phase123", "0.1.6")]
 [BepInDependency("StreamToEarn_S2E_SupermarketSimulator", BepInDependency.DependencyFlags.HardDependency)]
 public sealed class OliverPhase123Plugin : BasePlugin
 {
